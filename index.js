53335134980c5fe72fe084300b18070070881999
LewisSin <137732215+LewisSin@users.noreply.github.com>
2024-03-11 20:18:23 -0400

    updated code to fit requirements

--- index.js
+++ index.js
@@ -1,61 +1,38 @@
-var inquirer = require("inquirer");
-var {circle, triangle, square} = require("./lib/shapes");
-var fs = require("fs");
+// LogoGenerator.js
 
-const questions = [
-    {
-        name: "text",
-        type: "input", 
-        message: "Enter up to 3 characters for your logo text:"
-    },
-    {
-        name: "textColor",
-        type: "input",
-        message: "Enter a color keyword or hex code for your text color:"
-    }, 
-    {
-        name: "shapeColor",
-        type: "input",
-        message: "Enter a color keyword or hex code for your shape color:"
-    },
-    {
-        name: "shape",
-        type: "list",
-        message: "Choose which shape you want your logo to be:",
-        choices: ["circle", "triangle", "square"]
-    }
-]
+const inquirer = require("inquirer");
+const fs = require("fs").promises;
+const { CircularShape, RectangularShape, TriangularShape } = require("./lib/shapes");
 
-function writeToFile(filename, data) {
-fs.writeFile(filename, data, function(err) {
-    if (err) {
-        return console.log("error");
+const prompts = [
+  { name: "label", type: "input", message: "Enter up to 3 characters for your logo label:" },
+  { name: "labelColor", type: "input", message: "Enter a color keyword or hex code for the label color:" },
+  { name: "fillColor", type: "input", message: "Enter a color keyword or hex code for the background color:" },
+  { name: "shapeType", type: "list", message: "Choose the shape for your logo:", choices: ["Circular", "Triangular", "Rectangular"] }
+];
+
+async function initiateLogoCreation() {
+  try {
+    const responses = await inquirer.prompt(prompts);
+    let svgContent;
+
+    switch (responses.shapeType) {
+      case "Circular":
+        svgContent = new CircularShape(responses.label, responses.labelColor, responses.fillColor).render();
+        break;
+      case "Triangular":
+        svgContent = new TriangularShape(responses.label, responses.labelColor, responses.fillColor).render();
+        break;
+      case "Rectangular":
+        svgContent = new RectangularShape(responses.label, responses.labelColor, responses.fillColor).render();
+        break;
     }
-    console.log("generated logo.svg");
-})
+
+    await fs.writeFile("logo.svg", svgContent);
+    console.log("Generated logo.svg successfully.");
+  } catch (error) {
+    console.error("An error occurred:", error);
+  }
 }
 
-async function shapeRender() {
-
-    inquirer.prompt(questions).then(function(data) {
-        if (data.text.length > 3) {
-            console.log("error. Invalid text length");
-        }
-        else if (data.shape === "circle") {
-            let svgCircle = new circle(data.text, data.textColor, data.shapeColor);
-            writeToFile("./examples/logo.svg", svgCircle.render())
-        }
-
-        else if (data.shape === "triangle") {
-            let svgTriangle = new triangle(data.text, data.textColor, data.shapeColor);
-            writeToFile("./examples/logo.svg", svgTriangle.render())
-        }
-
-        else if (data.shape === "square") {
-            let svgSquare = new square(data.text, data.textColor, data.shapeColor);
-            writeToFile("./examples/logo.svg", svgSquare.render())
-        }
-    });
-}
-
-shapeRender();
+initiateLogoCreation();
